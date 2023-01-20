3601d5488bb6148235f19c3d17ab2381df1015a6
庄司 倫太郎 <shojirintaro@gmail.com>
2023-01-20 19:30:37 +0900

    線風源の表示を修正した

--- CEffect_Wind.cpp
+++ CEffect_Wind.cpp
@@ -3,8 +3,8 @@
 #include <DxLib.h>
 
 CEffect_Wind::CEffect_Wind(CVector position, float power, float angle, float width, float length)
-	:CEffect(position),basePos(position), duration(length / power / 0.25), power(power*0.25), angle(angle), width(width), length(length),
-	delta(GetRand(100) * 0.01 * Constant::PI2),x(0), y(0), z(0)
+	:CEffect(position), basePos(position), duration(length / power / 0.25), power(power * 0.25), angle(angle), width(width), length(length),
+	delta(GetRand(100) * 0.01 * Constant::PI2), x(0), y(0), z(0)
 {
 
 }
@@ -15,10 +15,10 @@ void CEffect_Wind::Update()
 		Status = 1;
 		return;
 	}
-	z = Constant::PI2 * Constant::perFrame * (duration/4) * cnt;
+	z = Constant::PI2 * Constant::perFrame * (duration / 4) * cnt;
 	x = cos(z + delta) * width * 0.5;
 	y = cnt * power;
-	z = sin(z + delta);
+	z = (sin(z + delta) + 1.0) * 0.5;
 	Position.x = cos(angle) * y + cos(angle + Constant::PI / 2) * x;
 	Position.y = sin(angle) * y + sin(angle + Constant::PI / 2) * x;
 	Position += basePos;
@@ -27,8 +27,8 @@ void CEffect_Wind::Update()
 
 void CEffect_Wind::Render() const
 {
-	CImageManager::getIns().find("effect_wind")->DrawRotaFwithBlend(Position.x, Position.y, 
-		angle, ((double)(duration - cnt) / duration*0.5 + 0.5) / 6.0, 
-		0xFFFFFF, CImageManager::BLENDMODE::BM_ADD, z * 0xFF,
+	CImageManager::getIns().find("effect_wind")->DrawRotaFwithBlend(Position.x, Position.y,
+		angle, ((double)(duration - cnt) / duration * 0.5 + 0.5) / 6.0,
+		0xFFFFFF, CImageManager::BLENDMODE::BM_ALPHA, z * 0xFF,
 		Constant::priority_effect, 1);
 }

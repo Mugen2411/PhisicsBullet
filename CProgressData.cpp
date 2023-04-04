88a2ba929ade89326ea6cf76e4166a46e638c5a1
庄司 倫太郎 <shojirintaro@gmail.com>
2023-04-04 17:09:54 +0900

    ステージ18を追加。

--- CProgressData.cpp
+++ CProgressData.cpp
@@ -8,7 +8,7 @@
 
 CProgressData::CProgressData()
     : current_stage_(0),
-      max_stage_(17),
+      max_stage_(18),
       earned_money_(0),
       data_{0, 0, 0, 0},
       is_endless_(false),
